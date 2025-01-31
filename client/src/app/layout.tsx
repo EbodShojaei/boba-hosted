35d465a29c7e82b16427f201ac3187b2461b98cd
Ebod Shojaei <ebod.shojaei@gmail.com>
2025-01-30 20:44:04 -0800

    (add) Next.js analytics 📊

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/shared/Providers";
@@ -48,6 +50,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <GoogleAnalytics />
+        <Analytics />
         <CookieConsentBanner />
         <ThemeProvider
           attribute="class"
